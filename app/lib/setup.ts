0da844966547bb5688e3df20a3f3f3a1ab981feb
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2023-08-31 09:11:01 +0200

    :fire: Remove no longer needed code to parse and load excel files

--- app/lib/setup.ts
+++ app/lib/setup.ts
@@ -1,12 +1,5 @@
-import { writeFileSync } from "fs";
-import loadPlaceOriginDeath from "./loadPlaceOriginDeath";
 import { setupDb } from "./setup-db";
 
 (async () => {
-  const data = await loadPlaceOriginDeath();
-  writeFileSync(
-    "app/data/placeOriginDeath.json",
-    JSON.stringify(data.objects())
-  );
   await setupDb();
 })();
