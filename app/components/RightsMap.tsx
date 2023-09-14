007161305e1ec3bd3e57dd593faef7cd72e09ffd
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2023-09-14 17:33:10 +0200

    :alembic: Use maplibre for rights map

--- app/components/RightsMap.tsx
+++ app/components/RightsMap.tsx
@@ -1,15 +1,11 @@
 "use client";
 
+import Map, { Marker, NavigationControl } from "react-map-gl/maplibre";
 import { FeatureCollection, GeoJsonProperties, Point } from "geojson";
-import dynamic from "next/dynamic";
 import { FC, useState } from "react";
-import { Marker, MarkerLayer } from "react-leaflet-marker";
 import Snowflake from "./Snowflake";
 import colorScaleAnsbach from "../lib/colorScaleAnsbach";
-
-const DynamicMap = dynamic(() => import("../components/Map"), {
-  ssr: false,
-});
+import "maplibre-gl/dist/maplibre-gl.css";
 
 type Props = {
   data: FeatureCollection<Point, GeoJsonProperties>;
@@ -20,48 +16,41 @@ const RightsMap: FC<Props> = ({ data }) => {
     undefined
   );
   return (
-    <>
-      <DynamicMap data={data.features}>
-        <MarkerLayer>
-          {data.features.map((d) => {
-            const radius = 20;
-            const margin = 2;
-            const markerSize = radius * 4 + margin;
-            return (
-              <div className="absolute" key={d.properties?.id}>
-                <Marker
-                  position={
-                    [...d.geometry.coordinates].reverse() as [number, number]
-                  }
-                  size={[markerSize, markerSize]}
-                  placement="center"
-                  interactive
-                  riseOnHover
-                  zIndexOffset={100}
-                >
-                  <svg width={markerSize} height={markerSize}>
-                    <g
-                      transform={`translate(${markerSize / 2} ${
-                        markerSize / 2
-                      })`}
-                    >
-                      <Snowflake
-                        placeName={d.properties?.place}
-                        placeAttributes={d.properties?.attributes}
-                        radius={radius}
-                        activeCategory={activeCategory}
-                        handleCategoryClick={setActiveCategory}
-                        colorScale={colorScaleAnsbach}
-                      />
-                    </g>
-                  </svg>
-                </Marker>
-              </div>
-            );
-          })}
-        </MarkerLayer>
-      </DynamicMap>
-    </>
+    <Map
+      initialViewState={{
+        longitude: 10.5,
+        latitude: 49.3,
+        zoom: 12,
+      }}
+      //@ts-expect-error
+      className={"w-full h-full"}
+      mapStyle="https://basemap.de/data/produkte/web_vektor/styles/bm_web_bin.json"
+    >
+      <NavigationControl />
+      {data.features.map((d) => {
+        const radius = 30;
+        const markerSize = radius * 2.5;
+        return (
+          <Marker
+            longitude={d.geometry.coordinates[0]}
+            latitude={d.geometry.coordinates[1]}
+          >
+            <svg width={markerSize} height={markerSize}>
+              <g transform={`translate(${markerSize / 2} ${markerSize / 2})`}>
+                <Snowflake
+                  placeName={d.properties?.place}
+                  placeAttributes={d.properties?.attributes}
+                  radius={radius}
+                  activeCategory={activeCategory}
+                  handleCategoryClick={setActiveCategory}
+                  colorScale={colorScaleAnsbach}
+                />
+              </g>
+            </svg>
+          </Marker>
+        );
+      })}
+    </Map>
   );
 };
 
