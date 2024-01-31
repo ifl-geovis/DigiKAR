fb90257c59816465677f34efb3a06b129c15b69d
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2024-01-31 16:55:50 +0100

    :bug: Use readonly connections for client queries

--- lib/createDatabase.ts
+++ lib/createDatabase.ts
@@ -1,7 +1,7 @@
-import { Database } from "duckdb-async";
+import { Database, OPEN_READONLY } from "duckdb-async";
 
 export const createDatabase = async () => {
-  const db = await Database.create("./data/digikar.duckdb");
+  const db = await Database.create("./data/digikar.duckdb", OPEN_READONLY);
   await db.run(`SET extension_directory="./data";`);
   await db.run("LOAD spatial;");
   return db;
