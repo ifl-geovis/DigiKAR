6cda6bc939aa31b860c97ed7dd3e46427ee98f2d
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2024-08-29 08:05:00 +0000

    ⚰️ Remove deprecated comment

--- lib/getUniquePlaces.ts
+++ lib/getUniquePlaces.ts
@@ -4,7 +4,6 @@ export const getUniquePlaces = async (
   /** Which data source to be used */
   source?: string,
 ) => {
-  //TODO: to avoid white listing table names, create view with all places and source as column
   const db = await getDatabase();
   const connection = await db.connect();
 
