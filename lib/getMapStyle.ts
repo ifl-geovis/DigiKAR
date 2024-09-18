99bb1134b809655853cff7663eaf897a7006d162
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2024-09-18 19:26:53 +0000

    ✨ Add new map style using martin tiles

--- lib/getMapStyle.ts
+++ lib/getMapStyle.ts
@@ -2,7 +2,7 @@ import { promises as fs } from "fs";
 import { StyleSpecification } from "maplibre-gl";
 
 export const getMapStyle = async () => {
-  const file = await fs.readFile("lib/mapStyle.json", "utf-8");
+  const file = await fs.readFile("lib/digikar-style.json", "utf-8");
   const style = (await JSON.parse(file)) as StyleSpecification;
   return style;
 };
