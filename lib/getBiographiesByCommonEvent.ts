06cd7f2ba29f3f772d03d4040067bedbffb8061a
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2025-01-08 16:17:29 +0000

    🚧 Get separate feature per biography event pair

--- lib/getBiographiesByCommonEvent.ts
+++ lib/getBiographiesByCommonEvent.ts
@@ -10,67 +10,97 @@ export const getBiographiesByCommonEvent = async (
 
   //TODO: remove where condition on lens as soon as student lens has person_id
   const statement = await connection.prepare(`
-    WITH ordered_events AS (
+    WITH initial_events AS (
       FROM events
-      WHERE 
-        person_id IN (
-          SELECT DISTINCT person_id
-          FROM events
-          WHERE 
-            event_type = ?
-            AND place_name ILIKE ?
-            AND event_analytical_lens <> 'students'
-          )
-        AND place_name IS NOT NULL
-      ORDER BY event_date
+      WHERE
+        event_analytical_lens <> 'students'
+        AND place IS NOT NULL
+        AND event_type = ?
+        AND place_name ILIKE ?
+    ),
+    ordered_events AS (
+      SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY event_value, event_date_start) AS rn,
+        COUNT(*) OVER (PARTITION BY person_id) AS total_events
+      FROM events
+      WHERE
+        person_id IN (SELECT DISTINCT person_id FROM initial_events)
+        AND place IS NOT NULL
+    ),
+    filtered_events AS (
+      FROM ordered_events
+      WHERE total_events >= 2
+    ),
+    events_with_next AS (
+      SELECT
+        *,
+        LEAD(event_type) OVER (PARTITION BY person_id ORDER BY rn) AS next_event_type,
+        LEAD(person_function) OVER (PARTITION BY person_id ORDER BY rn) AS next_person_function,
+        LEAD(event_value) OVER (PARTITION BY person_id ORDER BY rn) AS next_event_value,
+        LEAD(place_name) OVER (PARTITION BY person_id ORDER BY rn) AS next_place_name,
+        LEAD(event_date_start) OVER (PARTITION BY person_id ORDER BY rn) AS next_event_date_start,
+        LEAD(place) OVER (PARTITION BY person_id ORDER BY rn) AS next_place
+      FROM ordered_events
     )
-    SELECT 
-    json_object(
-          'type',
-          'Feature',
-          'properties',
+    SELECT json_object(
+      'type', 'Feature',
+      'properties', json_object(
+        'personId', person_id,
+        'name', person_name,
+        'order', rn,
+        'progress', (rn + 1) / total_events,
+        'events', json_array(
           json_object(
-              -- id
-              'id',
-              person_id,
-              'name',
-              FIRST(person_name),
-              -- events_array
-              'events',
-              json_group_array(
-                  json_object(
-                      'type', event_type,
-                      'functionality', person_function,
-                      'value', event_value,
-                      'place', place_name,
-                      'start', event_date_start
-                  )
-              )
+            'type', event_type,
+            'personFunction', person_function,
+            'eventValue', event_value,
+            'place', place_name,
+            'start', event_date_start
           ),
-          'geometry',
-          ST_AsGeoJSON(ST_MakeLine(LIST(place)))::JSON
-      ) AS feature
-    FROM ordered_events
-    GROUP BY person_id
-    HAVING COUNT(*) >= 2;
+          json_object(
+            'type', next_event_type,
+            'personFunction', next_person_function,
+            'eventValue', next_event_value,
+            'place', next_place_name,
+            'start', next_event_date_start
+          )
+        )
+      ),
+      'geometry', ST_AsGeoJSON(
+        ST_MakeLine(
+          place,
+          next_place
+        )
+      )
+    ) AS feature
+    FROM events_with_next
+    WHERE
+      next_event_type IS NOT NULL
+      AND total_events >= 2;
   `);
   const res = await statement.all(event, place);
+
   connection.close();
+
+  type BiographyEvent = {
+    type: string;
+    functionality: string;
+    value: string;
+    place: string;
+    start: string;
+  };
 
   return res.map(({ feature }) => {
     return JSON.parse(feature);
   }) as Feature<
     LineString,
     {
-      id: string;
+      personId: string;
       name: string;
-      events: {
-        type: string;
-        functionality: string;
-        value: string;
-        place: string;
-        start: string;
-      }[];
+      order: number;
+      progress: number;
+      events: [BiographyEvent, BiographyEvent];
     }
   >[];
 };
