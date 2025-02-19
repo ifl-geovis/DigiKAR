6c2306e344680824e04382ed7b5b89c440b922c6
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2025-02-19 11:58:09 +0100

    :bug: Fix name retrieval for undefined names

--- lib/getRightHolderNames.ts
+++ lib/getRightHolderNames.ts
@@ -11,13 +11,13 @@ const hasTypeAttribute = (
     | Perspectives["topLevels"],
 ): attribute is IndividualDatum[] => {
   return attribute?.every(
-    (d) => typeof d !== "string" && Object.hasOwn(d, "type"),
+    (d) => d && typeof d !== "string" && Object.hasOwn(d, "type"),
   );
 };
 
 export const getRightHolderNames = (holders: PerspectiveDatum[]) => {
   const names = hasTypeAttribute(holders)
     ? holders?.map((d) => d.name.normalize())
-    : holders?.map((d) => (d as string).normalize());
+    : holders?.map((d) => (d as string | undefined)?.normalize());
   return names;
 };
