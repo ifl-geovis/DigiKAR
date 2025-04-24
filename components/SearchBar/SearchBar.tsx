5444899c162d760e1635f5ac7efc1f51b94641f5
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2025-04-24 10:08:47 +0200

    :sparkles: Add search for places

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,10 +1,62 @@
-import { Input } from "../ui/input";
+"use client";
+
+import fetcher from "@/lib/fetcher";
+import { Point } from "geojson";
+import { LngLat } from "maplibre-gl";
+import { useEffect, useState } from "react";
+import { useMap } from "react-map-gl/maplibre";
+import useSWRImmutable from "swr/immutable";
+import { AutoComplete } from "../autocomplete";
+
+type Place = {
+  id: string;
+  geometry: Point;
+  label: string;
+  case_study: string;
+};
 
 const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [selectedValue, setSelectedValue] = useState<string>("");
+
+  const { data, isLoading } = useSWRImmutable<Place[]>(
+    `https://api.geohistoricaldata.org/digikar/orte?in_sample_regions=is.true&order=label.asc&label=ilike.*${searchValue}*`,
+    fetcher,
+  );
+  const items =
+    data?.map((d) => ({
+      value: d.id,
+      label: d.label,
+    })) ?? [];
+
+  const { rightsMap } = useMap();
+  useEffect(() => {
+    if (!selectedValue || !data) return;
+    const coordinates = data.find((d) => d.id == selectedValue)?.geometry
+      .coordinates;
+    const center = coordinates
+      ? new LngLat(coordinates[0], coordinates[1])
+      : undefined;
+    if (rightsMap && center) {
+      rightsMap.flyTo({
+        center,
+        zoom: 14,
+        speed: 3,
+      });
+    }
+  }, [selectedValue, data, rightsMap]);
+
   return (
-    <div className="rounded bg-white shadow-sm">
-      <Input disabled placeholder="Suche nach einem Rechteinhaber …" />
-    </div>
+    <AutoComplete
+      selectedValue={selectedValue}
+      onSelectedValueChange={setSelectedValue}
+      searchValue={searchValue}
+      onSearchValueChange={setSearchValue}
+      items={items ?? []}
+      isLoading={isLoading}
+      emptyMessage="Keine Orte gefunden"
+      placeholder="Ort suchen ..."
+    />
   );
 };
 
