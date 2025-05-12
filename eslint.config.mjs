75a916a9c6d9d74740ec670e3aba400d507224c9
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2025-05-12 06:28:12 +0200

    :arrow_up: Upgrade to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint-config-prettier",
+  ),
+  {
+    rules: {
+      "import/newline-after-import": ["warn", { count: 1 }],
+    },
+  },
+];
+
+export default eslintConfig;
