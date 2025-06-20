7c61fa47b2826eae5b70410806cad685a000af61
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2025-06-20 13:43:32 +0200

    :lipstick: Improve alignment in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,19 +5,20 @@ import { LuDatabaseBackup, LuGithub } from "react-icons/lu";
 const Footer: FC = () => {
   return (
     <footer className="flex items-center border-t px-5 py-3 text-xs">
-      <div className="flex gap-4">
+      <div className="flex items-center gap-4">
         <div>
           DigiKAR
           <time dateTime={new Date().toISOString()} className="ml-1">
             {new Date().getFullYear()}
           </time>
         </div>
-        <Link href="/rights/changelog" className="flex items-center gap-1">
-          <LuDatabaseBackup /> <span>Data Changelog</span>
-        </Link>
-        <div className="h-auto w-0 space-x-2 border-l border-gray-300" />
+        <div className="h-auto w-0 space-x-2 self-stretch border-l border-gray-300" />
         <Link href="https://github.com/ifl-geovis/DigiKAR">
           <LuGithub />
+        </Link>
+        <div className="h-auto w-0 space-x-2 self-stretch border-l border-gray-300" />
+        <Link href="/rights/changelog" className="flex items-center gap-1">
+          <LuDatabaseBackup /> <span>Data Changelog</span>
         </Link>
       </div>
       <ul className="ml-auto flex gap-4">
