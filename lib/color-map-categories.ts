24133dd9382aafc21245a5b292a59726105a91a4
Jakob Listabarth <j_listabarth@leibniz-ifl.de>
2025-06-21 07:01:30 +0200

    :pencil2: Fix category name

--- lib/color-map-categories.ts
+++ lib/color-map-categories.ts
@@ -1,6 +1,6 @@
 const colorMapCategories = new Map([
   ["geistliche Kurfürsten", "#4e0065"],
-  ["geistliche Fürsten und Prälate", "#8a3fa3"],
+  ["geistliche Fürsten und Prälaten", "#8a3fa3"],
   ["andere geistliche Reichsstände", "#c977e6"],
   ["weltliche Kurfürsten", "#007300"],
   ["weltliche Fürsten, Grafen, Herren", "#3ea64c"],
